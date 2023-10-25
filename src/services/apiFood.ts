8c0c562b49d8fbfc6338978f579d6414fe71b1de
pauloivoandrade <pauloivoasantos@gmail.com>
2023-10-25 09:51:36 -0300

    Api Food e Api Drinks

---
+++ src/services/apiFood.ts
@@ -0,0 +1,40 @@
+import { useState, useEffect } from 'react';
+
+export default function useFetchApiFood(
+  ingrediente: string,
+  nome: string,
+  primeiraletra: string,
+) {
+  const [apiIngredients, setApiIngredients] = useState({});
+  const [apiName, setApiName] = useState({});
+  const [apiFirstLetter, setApiFirstLetter] = useState({});
+
+  const fetchIngredientsData = async () => {
+    const response = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingrediente}`);
+    const ingredientData = await response.json();
+    setApiIngredients(ingredientData);
+  };
+  const fetchNameData = async () => {
+    const response = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${nome}`);
+    const nomeData = await response.json();
+    setApiName(nomeData);
+  };
+  const fetchFirstLetter = async () => {
+    const response = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=${primeiraletra}`);
+    const letterData = await response.json();
+    setApiFirstLetter(letterData);
+  };
+
+  useEffect(() => {
+    fetchFirstLetter();
+    fetchIngredientsData();
+    fetchNameData();
+  }, []);
+  console.log(fetchFirstLetter());
+
+  return {
+    apiIngredients,
+    apiFirstLetter,
+    apiName,
+  };
+}
