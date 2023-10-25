1c62cf7203ab4f4864bf6c21028fb5eb0277bcba
pauloivoandrade <pauloivoasantos@gmail.com>
2023-10-25 10:31:54 -0300

    Feat: fetch functions

--- src/services/apiDrinks.ts
+++ src/services/apiDrinks.ts
@@ -1,40 +1,15 @@
-import { useState, useEffect } from 'react';
-
-export default function useFetchApiDrinks(
-  ingrediente: string,
-  nome: string,
-  primeiraletra: string,
-) {
-  const [apiIngredients, setApiIngredients] = useState({});
-  const [apiName, setApiName] = useState({});
-  const [apiFirstLetter, setApiFirstLetter] = useState({});
-
-  const fetchIngredientsData = async () => {
-    const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${ingrediente}`);
-    const ingredientData = await response.json();
-    setApiIngredients(ingredientData);
-  };
-  const fetchNameData = async () => {
-    const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${nome}`);
-    const nomeData = await response.json();
-    setApiName(nomeData);
-  };
-  const fetchFirstLetter = async () => {
-    const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${primeiraletra}`);
-    const letterData = await response.json();
-    setApiFirstLetter(letterData);
-  };
-
-  useEffect(() => {
-    fetchFirstLetter();
-    fetchIngredientsData();
-    fetchNameData();
-  }, []);
-  console.log(fetchFirstLetter());
-
-  return {
-    apiIngredients,
-    apiFirstLetter,
-    apiName,
-  };
+export async function fetchDrinkIngredientsData(ingrediente: string) {
+  const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${ingrediente}`);
+  const ingredientData = await response.json();
+  return ingredientData;
+}
+export async function fetchDrinkNameData(nome: string) {
+  const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${nome}`);
+  const nomeData = await response.json();
+  return nomeData;
+}
+export async function fetchDrinkFirstLetter(primeiraLetra: string) {
+  const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${primeiraLetra}`);
+  const letterData = await response.json();
+  return letterData;
 }
--- src/services/apiFood.ts
+++ src/services/apiFood.ts
@@ -1,64 +1,15 @@
-import { useState, useEffect } from 'react';
-
-export default function useFetchApiFood(
-  ingrediente: string,
-  nome: string,
-  primeiraletra: string,
-) {
-  const [apiIngredients, setApiIngredients] = useState({});
-  const [apiName, setApiName] = useState({});
-  const [apiFirstLetter, setApiFirstLetter] = useState({});
-
-  const fetchIngredientsData = async () => {
-    const response = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingrediente}`);
-    const ingredientData = await response.json();
-    setApiIngredients(ingredientData);
-  };
-  const fetchNameData = async () => {
-    const response = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${nome}`);
-    const nomeData = await response.json();
-    setApiName(nomeData);
-  };
-  const fetchFirstLetter = async () => {
-    const response = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=${primeiraletra}`);
-    const letterData = await response.json();
-    setApiFirstLetter(letterData);
-  };
-
-  useEffect(() => {
-    fetchFirstLetter();
-    fetchIngredientsData();
-    fetchNameData();
-  }, []);
-  console.log(fetchFirstLetter());
-
-  return {
-    apiIngredients,
-    apiFirstLetter,
-    apiName,
-  };
+export async function fetchFoodIngredientsData(ingrediente: string) {
+  const response = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingrediente}`);
+  const ingredientData = await response.json();
+  return ingredientData;
 }
-// import React, { useState } from 'react';
-// import { useFetchIngredientsData } from './seuArquivoComOHook'; // Substitua com o caminho correto para o seu arquivo
-
-// function MeuComponente() {
-//   const [ingrediente, setIngrediente] = useState(''); // Estado para armazenar o valor do input
-//   const { apiIngredients } = useFetchIngredientsData(ingrediente);
-
-//   const handleInputChange = (event) => {
-//     setIngrediente(event.target.value); // Atualiza o estado com o valor do input
-//   };
-
-//   // Restante do seu componente com um input controlado
-//   return (
-//     <div>
-//       <input
-//         type="text"
-//         value={ingrediente}
-//         onChange={handleInputChange}
-//         placeholder="Digite o ingrediente"
-//       />
-//       {/* Renderiza os dados do API conforme necessário */}
-//     </div>
-//   );
-// }
+export async function fetchFoodNameData(nome: string) {
+  const response = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${nome}`);
+  const nomeData = await response.json();
+  return nomeData;
+}
+export async function fetchFoodFirstLetter(primeiraLetra: string) {
+  const response = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=${primeiraLetra}`);
+  const letterData = await response.json();
+  return letterData;
+}
