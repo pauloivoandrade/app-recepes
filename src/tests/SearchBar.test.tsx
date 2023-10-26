32bebdfd372afb86318236acfbb53786d0e1e16d
Michael Fernandes <111091339+michaelfernan@users.noreply.github.com>
2023-10-26 10:41:51 -0300

    test: SearchBar

--- src/tests/SearchBar.test.tsx
+++ src/tests/SearchBar.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
-import { MemoryRouter } from 'react-router-dom'; // Importe o MemoryRouter
+import { MemoryRouter } from 'react-router-dom';
 import SearchBar from '../components/SearchBar';
 
 describe('<SearchBar />', () => {
