07d48ed26b2a28729e63e12357b8eee1864883df
Dalmoddn <dalmoddn@gmail.com>
2023-10-31 14:19:24 -0300

    Fixing lint errors

--- src/tests/mocks/meals.ts
+++ src/tests/mocks/meals.ts
@@ -1,65 +1,21 @@
 const meals = {
   meals: [
     {
-      idMeal: '52977',
-      strMeal: 'Corba',
-      strDrinkAlternate: null,
-      strCategory: 'Side',
-      strArea: 'Turkish',
-      strInstructions: 'Pick through your lentils for any foreign debris, rinse them 2 or 3 times, drain, and set aside.  Fair warning, this will probably turn your lentils into a solid block that you’ll have to break up later\r\nIn a large pot over medium-high heat, sauté the olive oil and the onion with a pinch of salt for about 3 minutes, then add the carrots and cook for another 3 minutes.\r\nAdd the tomato paste and stir it around for around 1 minute. Now add the cumin, paprika, mint, thyme, black pepper, and red pepper as quickly as you can and stir for 10 seconds to bloom the spices. Congratulate yourself on how amazing your house now smells.\r\nImmediately add the lentils, water, broth, and salt. Bring the soup to a (gentle) boil.\r\nAfter it has come to a boil, reduce heat to medium-low, cover the pot halfway, and cook for 15-20 minutes or until the lentils have fallen apart and the carrots are completely cooked.\r\nAfter the soup has cooked and the lentils are tender, blend the soup either in a blender or simply use a hand blender to reach the consistency you desire. Taste for seasoning and add more salt if necessary.\r\nServe with crushed-up crackers, torn up bread, or something else to add some extra thickness.  You could also use a traditional thickener (like cornstarch or flour), but I prefer to add crackers for some texture and saltiness.  Makes great leftovers, stays good in the fridge for about a week.',
-      strMealThumb: 'https://www.themealdb.com/images/media/meals/58oia61564916529.jpg',
-      strTags: 'Soup',
-      strYoutube: 'https://www.youtube.com/watch?v=VVnZd8A84z4',
-      strIngredient1: 'Lentils',
-      strIngredient2: 'Onion',
-      strIngredient3: 'Carrots',
-      strIngredient4: 'Tomato Puree',
-      strIngredient5: 'Cumin',
-      strIngredient6: 'Paprika',
-      strIngredient7: 'Mint',
-      strIngredient8: 'Thyme',
-      strIngredient9: 'Black Pepper',
-      strIngredient10: 'Red Pepper Flakes',
-      strIngredient11: 'Vegetable Stock',
-      strIngredient12: 'Water',
-      strIngredient13: 'Sea Salt',
-      strIngredient14: '',
-      strIngredient15: '',
-      strIngredient16: '',
-      strIngredient17: '',
-      strIngredient18: '',
-      strIngredient19: '',
-      strIngredient20: '',
-      strMeasure1: '1 cup ',
-      strMeasure2: '1 large',
-      strMeasure3: '1 large',
-      strMeasure4: '1 tbs',
-      strMeasure5: '2 tsp',
-      strMeasure6: '1 tsp ',
-      strMeasure7: '1/2 tsp',
-      strMeasure8: '1/2 tsp',
-      strMeasure9: '1/4 tsp',
-      strMeasure10: '1/4 tsp',
-      strMeasure11: '4 cups ',
-      strMeasure12: '1 cup ',
-      strMeasure13: 'Pinch',
-      strMeasure14: ' ',
-      strMeasure15: ' ',
-      strMeasure16: ' ',
-      strMeasure17: ' ',
-      strMeasure18: ' ',
-      strMeasure19: ' ',
-      strMeasure20: ' ',
-      strSource: 'https://findingtimeforcooking.com/main-dishes/red-lentil-soup-corba/',
-      dateModified: null,
-    },
-    {
       idMeal: '52978',
       strMeal: 'Kumpir',
       strDrinkAlternate: null,
       strCategory: 'Side',
       strArea: 'Turkish',
-      strInstructions: 'If you order kumpir in Turkey, the standard filling is first, lots of butter mashed into the potato, followed by cheese. There’s then a row of other toppings that you can just point at to your heart’s content – sweetcorn, olives, salami, coleslaw, Russian salad, allsorts – and you walk away with an over-stuffed potato because you got ever-excited by the choices on offer.\r\n\r\nGrate (roughly – you can use as much as you like) 150g of cheese.\r\nFinely chop one onion and one sweet red pepper.\r\nPut these ingredients into a large bowl with a good sprinkling of salt and pepper, chilli flakes (optional).',
+      strInstructions: `'If you order kumpir 
+      in Turkey, the standard filling is first, lots of butter mashed into the potato, 
+      followed by cheese. There’s then a row of other toppings that you can just point 
+      at to your heart’s content – sweetcorn, olives, salami, coleslaw, Russian salad, 
+      allsorts – and you walk away with an over-stuffed potato 
+      because you got ever-excited by the choices on offer.
+      Grate (roughly – you can use as much as you like) 150g of cheese.
+       Finely chop one onion and one sweet red pepper.
+       Put these ingredients into a large bowl 
+      with a good sprinkling of salt and pepper, chilli flakes (optional).'`,
       strMealThumb: 'https://www.themealdb.com/images/media/meals/mlchx21564916997.jpg',
       strTags: 'SideDish',
       strYoutube: 'https://www.youtube.com/watch?v=IEDEtZ4UVtI',
@@ -112,7 +68,19 @@ const meals = {
       strDrinkAlternate: null,
       strCategory: 'Vegetarian',
       strArea: 'Indian',
-      strInstructions: 'Wash and soak toor dal in approx. 3 cups of water, for at least one hours. Dal will be double in volume after soaking. Drain the water.\r\nCook dal with 2-1/2 cups water and add salt, turmeric, on medium high heat, until soft in texture (approximately 30 mins) it should be like thick soup.\r\nIn a frying pan, heat the ghee. Add cumin seeds, and mustard seeds. After the seeds crack, add bay leaves, green chili, ginger and chili powder. Stir for a few seconds.\r\nAdd tomatoes, salt and sugar stir and cook until tomatoes are tender and mushy.\r\nAdd cilantro and garam masala cook for about one minute.\r\nPour the seasoning over dal mix it well and cook for another minute.\r\nServe with Naan.',
+      strInstructions: `'Wash and soak toor dal in approx. 3 cups of water,
+       for at least one hours. Dal will be double in volume after 
+       soaking. Drain the water.Cook dal with 2-1/2 cups water and add salt,
+        turmeric, on medium high heat, until soft in texture 
+        (approximately 30 mins) it should be like thick soup.
+        In a frying pan, heat the ghee. Add cumin seeds, and mustard 
+        seeds. After the seeds crack, add bay leaves, green chili, 
+        ginger and chili powder. Stir for a few seconds.
+        Add tomatoes, salt and sugar stir and cook until 
+        tomatoes are tender and mushy.Add cilantro and 
+        garam masala cook for about one minute.
+        Pour the seasoning over dal mix it well and cook for another minute.
+        Serve with Naan.'`,
       strMealThumb: 'https://www.themealdb.com/images/media/meals/wuxrtu1483564410.jpg',
       strTags: 'Curry,Vegetarian,Cake',
       strYoutube: 'https://www.youtube.com/watch?v=J4D855Q9-jg',
@@ -165,7 +133,12 @@ const meals = {
       strDrinkAlternate: null,
       strCategory: 'Miscellaneous',
       strArea: 'Canadian',
-      strInstructions: 'Heat oil in a deep fryer or deep heavy skillet to 365°F (185°C).\r\nWarm gravy in saucepan or microwave.\r\nPlace the fries into the hot oil, and cook until light brown, about 5 minutes.\r\nRemove to a paper towel lined plate to drain.\r\nPlace the fries on a serving platter, and sprinkle the cheese over them.\r\nLadle gravy over the fries and cheese, and serve immediately.',
+      strInstructions: `'Heat oil in a deep fryer or deep heavy skillet
+       to 365°F (185°C).Warm gravy in saucepan or microwave.
+       Place the fries into the hot oil, and cook until light brown,
+        about 5 minutes.Remove to a paper towel lined plate to drain.
+        Place the fries on a serving platter, and sprinkle the cheese over them.
+        Ladle gravy over the fries and cheese, and serve immediately.'`,
       strMealThumb: 'https://www.themealdb.com/images/media/meals/uuyrrx1487327597.jpg',
       strTags: 'UnHealthy,Speciality,HangoverFood',
       strYoutube: 'https://www.youtube.com/watch?v=UVAMAoA2_WU',
