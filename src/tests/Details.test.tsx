ebd6523e7a5985c98e2c1143d68a9098b30002e7
Paulo Ivo Andrade <132634934+pauloivoandrade@users.noreply.github.com>
2023-11-01 18:15:36 -0300

    Update Details.test.tsx

--- src/tests/Details.test.tsx
+++ src/tests/Details.test.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import 'matchmedia-polyfill';
 import { screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import App from '../App';
