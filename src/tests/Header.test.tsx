85002c24985a9e4addb01156abd34745eb59d56e
Michael Fernandes <111091339+michaelfernan@users.noreply.github.com>
2023-11-03 15:17:47 -0300

    test: DoneRecipes

--- src/tests/Header.test.tsx
+++ src/tests/Header.test.tsx
@@ -1,9 +1,12 @@
 import 'matchmedia-polyfill';
 import React from 'react';
-import { fireEvent, screen } from '@testing-library/react';
+import { fireEvent, screen, render, act } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
+import { describe, it, expect, vi, afterEach, beforeEach, SpyInstance } from 'vitest';
+import { MemoryRouter, Route, Routes } from 'react-router-dom';
 import App from '../App';
 import renderWithRouter from './helpers/renderWithRouter';
+import { DoneRecipes } from '../components/DoneRecipes';
 
 const PROFILE_BTN_TEST_ID = 'profile-top-btn';
 const PAGE_TITLE_TEST_ID = 'page-title';
@@ -89,5 +92,170 @@ describe('Header', () => {
   it('Rota "/drinks/[dynamic-part]/in-progress": não possui header', () => {
     renderWithRouter(<App />, { route: '/drinks/randomDrink/in-progress' });
     expect(screen.queryByTestId(PROFILE_BTN_TEST_ID)).not.toBeInTheDocument();
+  });
+
+  it('renders DoneRecipes without crashing', () => {
+    renderWithRouter(<DoneRecipes />);
+    expect(screen.getByTestId('filter-by-all-btn')).toBeInTheDocument();
+  });
+
+  it('filters recipes when filter buttons are clicked', () => {
+    const meals = [
+      { id: '1', type: 'meal', name: 'Meal1' },
+      { id: '2', type: 'meal', name: 'Meal2' },
+    ];
+    const drinks = [
+      { id: '3', type: 'drink', name: 'Drink1' },
+      { id: '4', type: 'drink', name: 'Drink2' },
+    ];
+    const doneRecipes = [...meals, ...drinks];
+    localStorage.setItem('doneRecipes', JSON.stringify(doneRecipes));
+
+    renderWithRouter(<DoneRecipes />);
+
+    fireEvent.click(screen.getByTestId('filter-by-meal-btn'));
+
+    meals.forEach((meal) => {
+      expect(screen.getByText(meal.name)).toBeInTheDocument();
+    });
+    drinks.forEach((drink) => {
+      expect(screen.queryByText(drink.name)).not.toBeInTheDocument();
+    });
+
+    fireEvent.click(screen.getByTestId('filter-by-drink-btn'));
+
+    drinks.forEach((drink) => {
+      expect(screen.getByText(drink.name)).toBeInTheDocument();
+    });
+    meals.forEach((meal) => {
+      expect(screen.queryByText(meal.name)).not.toBeInTheDocument();
+    });
+  });
+});
+
+describe('DoneRecipes component', () => {
+  const doneRecipesMock = [
+    {
+      id: '1',
+      type: 'meal',
+      nationality: 'Italian',
+      category: 'Pasta',
+      alcoholicOrNot: '',
+      name: 'Spaghetti Carbonara',
+      image: 'image-url',
+      doneDate: '2023-05-30',
+      tags: ['tag1', 'tag2'],
+    },
+
+  ];
+
+  let clipboardSpy: SpyInstance<[data: string], Promise<void>>;
+
+  beforeEach(() => {
+    vi.useFakeTimers();
+    localStorage.clear();
+    vi.spyOn(global.localStorage, 'getItem');
+    vi.spyOn(global.localStorage, 'setItem');
+    localStorage.setItem('doneRecipes', JSON.stringify(doneRecipesMock));
+    clipboardSpy = vi.spyOn(navigator.clipboard, 'writeText').mockResolvedValue();
+    vi.useFakeTimers();
+
+    localStorage.setItem('doneRecipes', JSON.stringify(doneRecipesMock));
+  });
+
+  afterEach(() => {
+    clipboardSpy.mockRestore();
+    vi.useRealTimers();
+    vi.restoreAllMocks();
+    localStorage.clear();
+  });
+
+  it('navigates to the recipe detail page when a recipe name is clicked', () => {
+    render(
+      <MemoryRouter initialEntries={ ['/done-recipes'] }>
+        <Routes>
+          <Route path="/done-recipes" element={ <DoneRecipes /> } />
+          <Route path="/meals/:id" element={ <p>Meal Detail Page</p> } />
+        </Routes>
+      </MemoryRouter>,
+    );
+
+    const recipeNameButton = screen.getByTestId('0-horizontal-name');
+    fireEvent.click(recipeNameButton);
+    expect(screen.getByText('Meal Detail Page')).toBeInTheDocument();
+  });
+
+  it('copies the correct URL to clipboard when share button is clicked', async () => {
+    render(<DoneRecipes />, { wrapper: MemoryRouter });
+    const shareButton = screen.getByTestId('0-horizontal-share-btn');
+    await act(async () => {
+      fireEvent.click(shareButton);
+    });
+    expect(clipboardSpy).toHaveBeenCalledWith('http://localhost:3000/meals/1');
+
+    expect(screen.getByText('Link copied!')).toBeInTheDocument();
+
+    act(() => {
+      vi.runAllTimers();
+    });
+    expect(screen.queryByText('Link copied!')).not.toBeInTheDocument();
+  });
+});
+
+describe('DoneRecipes 2', () => {
+  const doneRecipesMock2 = [
+    {
+      id: '1',
+      type: 'meal',
+      nationality: 'Italian',
+      category: 'Pasta',
+      alcoholicOrNot: '',
+      name: 'Spaghetti Carbonara',
+      image: 'image-url',
+      doneDate: '2023-04-30',
+      tags: ['tag1', 'tag2'],
+    }, {
+      id: '2',
+      type: 'drink',
+      nationality: '',
+      category: 'Cocktail',
+      alcoholicOrNot: 'Alcoholic',
+      name: 'Margarita',
+      image: 'image-url',
+      doneDate: '2023-04-30',
+      tags: ['tag1', 'tag2'],
+    },
+
+  ];
+  let clipboardSpy: SpyInstance<[data: string], Promise<void>>;
+
+  beforeEach(() => {
+    vi.useFakeTimers();
+    localStorage.clear();
+    vi.spyOn(global.localStorage, 'getItem');
+    vi.spyOn(global.localStorage, 'setItem');
+    localStorage.setItem('doneRecipes', JSON.stringify(doneRecipesMock2));
+    clipboardSpy = vi.spyOn(navigator.clipboard, 'writeText').mockResolvedValue();
+    vi.useFakeTimers();
+  });
+
+  afterEach(() => {
+    clipboardSpy.mockRestore();
+    vi.useRealTimers();
+    vi.restoreAllMocks();
+    localStorage.clear();
+  });
+
+  it('should set filter to "all" when the handleFilterAll function is triggered', () => {
+    render(<DoneRecipes />, { wrapper: MemoryRouter });
+
+    const filterAllButton = screen.getByTestId('filter-by-all-btn');
+    fireEvent.click(filterAllButton);
+
+    const mealItem = screen.getByText(doneRecipesMock2[0].name);
+    const drinkItem = screen.getByText(doneRecipesMock2[1].name);
+
+    expect(mealItem).toBeInTheDocument();
+    expect(drinkItem).toBeInTheDocument();
   });
 });
